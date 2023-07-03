e779854bff6f1d5af8ecea0133421670c6f484f3
Tatiana Tcyganova <tatiana.kv@gmail.com>
2023-07-03 22:26:29 +0300

    New excersise 21

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 21
+
+// Console.Write("Введите координату по оси оХ для третьей точки: ");
+// int x3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для третьей точки: ");
+// int y3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для третьей точки: ");
+// int z3 = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.Write("Введите координату по оси оX для второй точки: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для второй точки: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для второй точки: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату по оси оX для первой точки: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для первой точки: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для первой точки: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// double d = Math.Round(Math.Sqrt(Math.Pow(x3 - x2 - x1, 2) + Math.Pow(y3 - y2 - y1, 2) + Math.Pow(z3 - z2 - z1, 2)), 2);
+
+// Console.WriteLine($"Расстояние: {d}");
+
+// Math.Sqrt(число) => Math.Sqrt(25) = 5 => Sqrt = корень
+// Math.Pow(число, степень) => Math.Pow(2,3) => 2 в 3 степени: 2 * 2 * 2 => Pow - степень
+// Math.Round(число, количество знаков) => Math.Round(1.12345, 2) => 1.12 - округление
